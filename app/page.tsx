634b3c6179a54a1e65071c0154bd65c74acd5fd1
Armando Flores <aflores57@gmail.com>
2026-05-27 20:06:36 -0400

    Update message in Home component for clarity

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <main className="mx-auto w-full max-w-5xl">
         <Hero version={packageJson.version} />
       </main>
-      <p>Un cambio mas</p>
+      <p>Un cambio mas - quiza otro</p>
     </div>
   );
 }
